5b8a2b1ec5cc9bffcb7645a65dbe9a11a1fe721c
Matthew J. Lewis, Ph.D <lightscalar@gmail.com>
2017-06-06 23:01:54 -0400

    Removed unused functions.

--- dopamine/utils.py
+++ dopamine/utils.py
@@ -9,42 +9,6 @@ from dopamine.running_stats import *
 
 dtype='float32'
 
-
-class ZFilter(object):
-    """
-    y = (x-mean)/std
-    using running estimates of mean,std
-    """
-
-    def __init__(self, shape, demean=True, destd=True, clip=10.0):
-        self.demean = demean
-        self.destd = destd
-        self.clip = clip
-        self.rs = RunningStat(shape)
-
-    def __call__(self, x, update=True):
-        if update: self.rs.push(x)
-        if self.demean:
-            x = x - self.rs.mean
-        if self.destd:
-            x = x / (self.rs.std+1e-8)
-        if self.clip:
-            x = np.clip(x, -self.clip, self.clip)
-        return x
-
-    def output_shape(self, input_space):
-        return input_space.shape
-
-
-def filter_states(state, states):
-    '''Filter the states using a running mean/std.'''
-    if len(states)>0:
-        running_mean = np.mean(states)
-        running_std = np.std(states)
-        return (state - running_mean)/running_std
-    else:
-        return
-
 def discount(x, gamma):
     """
     computes discounted sums along 0th dimension of x.
